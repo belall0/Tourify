4c828c26eba7acecb7c57fafc717e410c000d0c4
Belal Muhammad <belallmuhammad0@gmail.com>
2024-11-30 00:31:53 +0200

    Implement access-based authorization using restrictTo middleware for tour routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -76,3 +76,15 @@ export const protectRoute = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+export const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new HttpError('You do not have permission to perform this action.', HttpStatus.FORBIDDEN),
+      );
+    }
+
+    next();
+  };
+};
