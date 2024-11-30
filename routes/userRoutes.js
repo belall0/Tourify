33c436d3ee8432f486f43b0a5917264ac3cfd61c
Belal Muhammad <belallmuhammad0@gmail.com>
2024-12-01 00:01:08 +0200

    feat: Implement updateUserProfile functionality to enable users to update their profile data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,7 @@ router.post('/login', authController.login);
 router.post('/forgot-password', authController.forgotPassword);
 router.patch('/reset-password/:token', authController.resetPassword);
 router.patch('/update-password', authController.protectRoute, authController.updatePassword);
+router.patch('/profile', authController.protectRoute, userController.updateCurrentUserProfile);
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router
