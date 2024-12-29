6ae71814ce30bb070707e89cbc603ff8445901d0
belal muhammad <belallmuhammad0@gmail.com>
2024-12-29 20:01:25 +0200

    feat: Update dbConnection.js to connect to MongoDB Atlas

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,10 +2,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const DATABASE_URL = process.env.DATABASE_URL;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_URI = process.env.DB_URI.replace('<DB_USERNAME>', DB_USERNAME).replace('<DB_PASSWORD>', DB_PASSWORD);
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(DATABASE_URL);
+    await mongoose.connect(DB_URI);
     console.log('Database connection established successfully!');
   } catch (error) {
     console.log('Error connecting to the database:', error.message);
